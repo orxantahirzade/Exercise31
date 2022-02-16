afa466eaac947de76bbd06c349b8f5c40efb418d
orxan.tahirzada@gmail.com <Azeorxan19>
2022-02-16 10:15:58 +0400

    First commit.

---
+++ src/ExerciseOOP/Human.java
@@ -0,0 +1,72 @@
+package ExerciseOOP;
+
+public class Human {
+
+    private String name;
+    private String surname;
+    private int age;
+    private Human father;
+    private Human mother;
+
+
+
+
+    @Override
+    public String toString() {
+        return "Human{" +
+                "name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", age=" + age +
+                ", father=" + father +
+                ", mother=" + mother +
+                '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Human getFather() {
+        return father;
+    }
+
+    public void setFather(Human father) {
+        this.father = father;
+    }
+
+    public Human getMother() {
+        return mother;
+    }
+
+    public void setMother(Human mother) {
+        this.mother = mother;
+    }
+
+    public Human(String name, String surname, int age, Human father, Human mother) {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+        this.father = father;
+        this.mother = mother;
+    }
+}
---
+++ src/ExerciseOOP/Person.java
@@ -0,0 +1,8 @@
+package ExerciseOOP;
+
+public class Person extends Human{
+
+    public Person(String name, String surname, int age, Human father, Human mother) {
+        super(name, surname, age, father, mother);
+    }
+}
